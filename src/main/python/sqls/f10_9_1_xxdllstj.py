4732cf8a63a723b479ccc82434d609d243d4257d
Chen <cs1318@tom.com>
2017-08-10 15:39:09 +0800

    同步语句加top，限制同步数据量

--- src/main/python/sqls/f10_9_1_xxdllstj.py
+++ src/main/python/sqls/f10_9_1_xxdllstj.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "Cyh"
-sql = """SELECT RsID, UpdateDateTime, Obj, zxDate AS ZxDate, count_num AS CountNum,
-       zxType AS ZxType
+sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsID, UpdateDateTime, Obj, zxDate AS ZxDate,
+       count_num AS CountNum, zxType AS ZxType
   FROM xxdllstj
  WHERE (   RsID > '{RsID}'
      AND   UpdateDateTime = '{UpdateDateTime}'
