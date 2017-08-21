faee96ac53b9ad99313eb548484a51d7a84e6538
Chen <cs1318@tom.com>
2017-08-21 18:10:54 +0800

    9.3.1~9.3.3信息地雷-指数-标题

---
+++ src/main/python/sqls/f10_9_3_1_zs_xxdlxw.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+__author__ = "Cyh"
+sql = """SELECT TOP 500 CAST(nm.SEQ AS NVARCHAR(MAX)) AS Rsid,
+       CONVERT( VARCHAR(30), nm.CTIME, 121 ) AS UpdateDateTime,
+       CONVERT( VARCHAR(30), nm.CTIME, 120 ) AS ZxDate,
+       CAST(nm.GUID AS NVARCHAR(20)) AS ZiXunId,
+       CAST(ncr.CLS_NAME AS NVARCHAR(50)) AS ZiXunType, 'SH000001' AS Obj,
+       '000001' AS StockCode, '指数' AS StockName,
+       CAST(nm.TITLE_MAIN AS NVARCHAR(300)) AS Title
+  FROM QKTZ20160429.dbo.NEWS_CLS AS ncs WITH ( NOLOCK )
+  INNER JOIN QKTZ20160429.dbo.NEWS_MAIN AS nm WITH ( NOLOCK )
+          ON nm.ISVALID = 1
+         AND nm.GUID = ncs.GUID
+  INNER JOIN QKTZ20160429.dbo.NEWS_CLS_REF AS ncr WITH ( NOLOCK )
+          ON ncr.ISVALID = 1
+         AND ncs.CLS_CODE = ncr.CLS_CODE
+ WHERE ncs.ISVALID = 1
+   AND LEFT(ncr.CLS_CODE, 3) IN (
+                                    '001', '002', '003', '004'
+                                )
+   AND (   nm.SEQ > '{@RsId}'
+     AND   nm.CTIME >= '{@UpdateDateTime}'
+       )
+ ORDER BY nm.MTIME, nm.SEQ"""
---
+++ src/main/python/sqls/f10_9_3_2_zs_xxdlgg.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+__author__ = "Cyh"
+sql = """SELECT TOP 500 CAST(dmc.SEQ AS NVARCHAR(MAX)) AS RsId,
+       CONVERT( NVARCHAR(30), dmc.CTIME, 121 ) AS UpdateDateTime,
+       CONVERT( NVARCHAR(30), dmc.CTIME, 120 ) AS ZxDate,
+       CAST(dmc.DISC_ID AS NVARCHAR(30)) AS ZiXunId,
+       CAST(dcr.CLS_NAME AS NVARCHAR(50)) AS ZiXunType, 'SH000001' AS Obj,
+       '000001' AS StockCode, '指数' AS StockName,
+       CAST(dmc.TITLE AS NVARCHAR(256)) AS Title
+  FROM QKTZ20160429.dbo.DISC_CLS_COM AS dcc WITH ( NOLOCK )
+  INNER JOIN QKTZ20160429.dbo.DISC_CLS_REF AS dcr WITH ( NOLOCK )
+          ON dcr.ISVALID = 1
+         AND dcc.CLS_CODE = dcr.CLS_CODE
+  INNER JOIN QKTZ20160429.dbo.DISC_MAIN_COM AS dmc WITH ( NOLOCK )
+          ON dmc.DISC_ID = dcc.DISC_ID
+         AND dmc.ISVALID = 1
+ WHERE dcc.ISVALID = 1
+   AND LEFT(dcr.CLS_CODE, 3) IN (
+                                    '007', '008'
+                                )
+   AND (   dmc.CTIME >= '{@UpdateDateTime}'
+     AND   dmc.SEQ > '{@RsId}'
+       )
+ ORDER BY dmc.CTIME, dmc.SEQ"""
