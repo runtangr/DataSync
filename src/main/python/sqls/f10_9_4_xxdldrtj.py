ea54397a5a1ccd94ae82ce6ab6e967e20d257da0
tangrui <1048393815@qq.com>
2017-08-21 15:23:57 +0800

    modify sql stocksoftdata

--- src/main/python/sqls/f10_9_4_xxdldrtj.py
+++ src/main/python/sqls/f10_9_4_xxdldrtj.py
@@ -3,7 +3,7 @@
 __author__ = "Cyh"
 sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsID, UpdateDateTime, Obj, ZxDate, CountNum,
        ZxType
-  FROM F10_9_4_XXDLDRTJ
+  FROM stocksoftdata.dbo.F10_9_4_XXDLDRTJ
  WHERE (   RsID > '{RsId}'
      AND   UpdateDateTime >= '{UpdateDateTime}'
        )"""
