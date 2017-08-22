65dc05e4465eea1a2e2f5608fb0c8d1d0e0cb885
Chen <cs1318@tom.com>
2017-08-22 18:00:54 +0800

    9.1数据源从库QKTZ20160429切换为库stocksoftdata

--- src/main/python/sqls/f10_9_1_xxdllstj.py
+++ src/main/python/sqls/f10_9_1_xxdllstj.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# 库：QKTZ20160429
+# 库：stocksoftdata
 __author__ = "Cyh"
 sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsID, UpdateDateTime, Obj, zxDate AS ZxDate,
        count_num AS CountNum, zxType AS ZxType
-  FROM xxdllstj
+  FROM stocksoftdata.dbo.xxdllstj
  WHERE (   RsID > '{RsId}'
      AND   UpdateDateTime >= '{UpdateDateTime}'
        )
