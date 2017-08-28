6debf3eb3a3e76118b8cb964e7dbd73055b1aad3
Chen <cs1318@tom.com>
2017-08-28 16:19:58 +0800

    修改9.3.1~9.3.3同步语句

--- src/main/python/sqls/f10_9_3_2_xxdlgg.py
+++ src/main/python/sqls/f10_9_3_2_xxdlgg.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
-__author__ = "Sommily"
-sql = """SELECT TOP 500 CAST(dco.SEQ AS VARCHAR) AS RsId,
-       CONVERT( VARCHAR(30), dco.MTIME, 126 ) AS UpdateDateTime,
-       CONVERT( VARCHAR(30), dco.MTIME, 120 ) AS ZxDate,QKTZ20160429.dbo.Date2Str(dco.MTIME) AS II_ZxDate, dco.DISC_ID AS ZiXunId,
-       'GG' AS ZiXunType, CASE
-                              WHEN stk.TRADE_MKT_REF = 1 THEN 'SZ'
-                              WHEN stk.TRADE_MKT_REF = 2 THEN 'SH'
-                              ELSE CAST(stk.TRADE_MKT_REF AS VARCHAR) + '-'
-                          END + stk.STOCKCODE AS Obj, stk.STOCKCODE AS StockCode,
-       stk.STOCKSNAME AS StockName, nm.TITLE AS Title
-  FROM DISC_COM AS dco WITH ( NOLOCK )
-  INNER JOIN STK_CODE AS stk WITH ( NOLOCK )
-          ON stk.ISVALID = 1
-         AND stk.COMCODE = dco.COMCODE
-  INNER JOIN DISC_MAIN_COM AS nm
-          ON nm.ISVALID = 1
-         AND dco.DISC_ID = nm.DISC_ID
- WHERE dco.ISVALID = 1
-   AND dco.DECLAREDATE >= CAST(CAST(YEAR( GETDATE()) - 2 AS VARCHAR) + '-01-01' AS DATETIME)
-   AND ( (   dco.MTIME = '{UpdateDateTime}'
-       AND   dco.SEQ > '{RsId}'
-        )
-      OR ( dco.MTIME > '{UpdateDateTime}' )
+__author__ = "Cyh"
+sql = """
+SELECT RsId, StockCode, Obj, StockName, ZiXunId, Title, ZiXunType, ZxDate, II_ZxDate,
+       DECLAREDATE, II_DECLAREDATE, MyDate, II_MyDate, UpdateDateTime, Status
+  FROM stocksoftdata.dbo.F10_9_3_2_XXDLGG
+ WHERE (   RsId > '{RsId}'
+     AND   UpdateDateTime = '{UpdateDateTime}'
        )
- ORDER BY dco.MTIME, dco.SEQ"""
+    OR UpdateDateTime > '{UpdateDateTime}'
+"""
