10a22011ab07603cc95218e4f17171599ad94174
tangrui <1048393815@qq.com>
2017-08-30 10:09:32 +0800

    modify 9_1

--- src/main/python/sqls/f10_9_1_xxdllstj.py
+++ src/main/python/sqls/f10_9_1_xxdllstj.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = "Cyh"
-sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsID, UpdateDateTime, Obj, zxDate AS ZxDate,stocksoftdata.dbo.Date2Str( zxDate ) AS II_ZxDate,
+sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsId, UpdateDateTime, Obj, zxDate AS ZxDate,stocksoftdata.dbo.Date2Str( zxDate ) AS II_ZxDate,
        count_num AS CountNum, zxType AS ZxType
   FROM stocksoftdata.dbo.xxdllstj
- WHERE (   RsID > '{RsId}'
+ WHERE (   RsId > '{RsId}'
      AND   UpdateDateTime >= '{UpdateDateTime}'
        )"""
