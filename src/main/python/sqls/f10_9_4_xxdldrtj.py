5d93437b3f8bf13f31d1f13f07193bdc41735762
tangrui <1048393815@qq.com>
2017-08-30 15:19:18 +0800

    modify

--- src/main/python/sqls/f10_9_4_xxdldrtj.py
+++ src/main/python/sqls/f10_9_4_xxdldrtj.py
@@ -2,7 +2,7 @@
 # 库：stocksoftdata 
 __author__ = "Cyh"
 sql = """SELECT TOP 500 CAST(RsID AS NVARCHAR(MAX)) AS RsId, UpdateDateTime, Obj, ZxDate,stocksoftdata.dbo.Date2Str( ZxDate ) AS II_ZxDate,
-       CountNum, ZxType
+       CountNum, ZxType,Status
   FROM stocksoftdata.dbo.F10_9_4_XXDLDRTJ
  WHERE (   RsId > '{RsId}'
      AND   UpdateDateTime = '{UpdateDateTime}'
