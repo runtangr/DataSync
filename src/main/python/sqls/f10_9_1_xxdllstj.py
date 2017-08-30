0ec0a41f8ef4af6f8656679c4ab69592e9edf420
Chen <cs1318@tom.com>
2017-08-30 09:58:57 +0800

    修改{RsID}为{RsId}

--- src/main/python/sqls/f10_9_1_xxdllstj.py
+++ src/main/python/sqls/f10_9_1_xxdllstj.py
@@ -3,6 +3,6 @@ __author__ = "Cyh"
 sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsID, UpdateDateTime, Obj, zxDate AS ZxDate,stocksoftdata.dbo.Date2Str( zxDate ) AS II_ZxDate,
        count_num AS CountNum, zxType AS ZxType
   FROM xxdllstj
- WHERE (   RsID > '{RsID}'
+ WHERE (   RsID > '{RsId}'
      AND   UpdateDateTime >= '{UpdateDateTime}'
        )"""
