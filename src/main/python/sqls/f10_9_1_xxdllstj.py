14828ad2e6c6f1ac95eaf79e2996d6d482364a2c
Chen <cs1318@tom.com>
2017-08-30 10:02:26 +0800

    9.1加库名

--- src/main/python/sqls/f10_9_1_xxdllstj.py
+++ src/main/python/sqls/f10_9_1_xxdllstj.py
@@ -2,7 +2,7 @@
 __author__ = "Cyh"
 sql = """SELECT TOP 500 CAST(RsID AS VARCHAR) AS RsID, UpdateDateTime, Obj, zxDate AS ZxDate,stocksoftdata.dbo.Date2Str( zxDate ) AS II_ZxDate,
        count_num AS CountNum, zxType AS ZxType
-  FROM xxdllstj
+  FROM stocksoftdata.dbo.xxdllstj
  WHERE (   RsID > '{RsId}'
      AND   UpdateDateTime >= '{UpdateDateTime}'
        )"""
