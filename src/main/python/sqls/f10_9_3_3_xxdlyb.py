a395566443f74cf284417c00bf5651dc894e62ac
Chen <cs1318@tom.com>
2017-09-01 16:23:45 +0800

    9.3.1~9.3.3字段名称规范化DECLAREDATE AS Declaredate, II_DECLAREDATE AS II_Declaredate

--- src/main/python/sqls/f10_9_3_3_xxdlyb.py
+++ src/main/python/sqls/f10_9_3_3_xxdlyb.py
@@ -2,7 +2,7 @@
 __author__ = "Cyh"
 sql = """
 SELECT RsId, StockCode, Obj, StockName, ZiXunId, Title, ZiXunType, ZxDate, II_ZxDate,
-       DECLAREDATE, II_DECLAREDATE, MyDate, II_MyDate, UpdateDateTime, Status
+       DECLAREDATE AS Declaredate, II_DECLAREDATE AS II_Declaredate,  MyDate, II_MyDate, UpdateDateTime, Status
   FROM stocksoftdata.dbo.F10_9_3_3_XXDLYB
  WHERE (   RsId > '{RsId}'
      AND   UpdateDateTime = '{UpdateDateTime}'
