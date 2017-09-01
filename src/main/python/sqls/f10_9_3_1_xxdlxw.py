96cd49478b5249d4b5fe66e9d4812c6b62a260e6
Chen <cs1318@tom.com>
2017-09-01 16:23:45 +0800

    9.3.1~9.3.3字段名称规范化DECLAREDATE AS Declaredate, II_DECLAREDATE AS II_Declaredate,同时同步语句改为存储过程调用

--- src/main/python/sqls/f10_9_3_1_xxdlxw.py
+++ src/main/python/sqls/f10_9_3_1_xxdlxw.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 __author__ = "Cyh"
 sql = """
-SELECT RsId, StockCode, Obj, StockName, ZiXunId, Title, ZiXunType, ZxDate, II_ZxDate,
-       DECLAREDATE, II_DECLAREDATE, MyDate, II_MyDate, UpdateDateTime, Status
-  FROM stocksoftdata.dbo.F10_9_3_1_XXDLXW
- WHERE (   RsId > '{RsId}'
-     AND   UpdateDateTime = '{UpdateDateTime}'
-       )
-    OR UpdateDateTime > '{UpdateDateTime}'
+EXEC poc_to_mdb_f10_9_3_1_xxdlxw @RsId = '{RsId}', @UpdateDateTime = '{UpdateDateTime}'
 """
--- src/main/python/sqls/f10_9_3_3_xxdlyb.py
+++ src/main/python/sqls/f10_9_3_3_xxdlyb.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 __author__ = "Cyh"
 sql = """
-SELECT RsId, StockCode, Obj, StockName, ZiXunId, Title, ZiXunType, ZxDate, II_ZxDate,
-       DECLAREDATE, II_DECLAREDATE, MyDate, II_MyDate, UpdateDateTime, Status
-  FROM stocksoftdata.dbo.F10_9_3_3_XXDLYB
- WHERE (   RsId > '{RsId}'
-     AND   UpdateDateTime = '{UpdateDateTime}'
-       )
-    OR UpdateDateTime > '{UpdateDateTime}'
+EXEC poc_to_mdb_f10_9_3_3_xxdlyb @RsId = '{RsId}', @UpdateDateTime = '{UpdateDateTime}'
 """
