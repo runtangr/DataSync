708f01b2bac2a0bb61ce4fdb7d470f6bc63dbbd9
tangrui <1048393815@qq.com>
2017-09-05 17:52:16 +0800

    modify 9_4

--- src/main/python/Application/f10_9_4_xxdldrtj_sync.py
+++ src/main/python/Application/f10_9_4_xxdldrtj_sync.py
@@ -16,38 +16,5 @@ class TableGetData(SyncBase.GetDataFromMssql):
     pass
 
 class TableSaveData(SyncBase.SaveDataToDB):
-    def SaveData(self, table_name=None, data=None):
-        # select
-        while True:
-            try:
-                count = self.db[table_name].find({"RsId": data["RsId"]}).count()
-                break
-            except pymongo.errors.AutoReconnect, e:
-                logging.error('AutoReconnect fail\n')
-                time.sleep(2)
-        if int(data["Status"]) == -1:
-
-            self.db[table_name].remove({"RsId": long(data["RsId"])})
-        else:
-
-            if count > 0:
-                # edit
-                try:
-                    # logging
-                    print("Edit ：RsId = {0} Timestamp = {1}".format(data["RsId"], data["UpdateDateTime"]))
-                    logging.info("Edit ：RsId = {0} Timestamp = {1}".format(data["RsId"], data["UpdateDateTime"]))
-                    self.db[table_name].update({"RsId": data["RsId"]}, {"$set": data})
-                except pymongo.errors.AutoReconnect, e:
-                    logging.error('AutoReconnect fail\n')
-                    time.sleep(2)
-            else:
-                # add
-                # logging
-                try:
-                    print("Add ：RsId = {0} Timestamp = {1}".format(data["RsId"], data["UpdateDateTime"]))
-                    logging.info("Add ：RsId = {0} Timestamp = {1}".format(data["RsId"], data["UpdateDateTime"]))
-                    self.db[table_name].insert(data)
-                except pymongo.errors.AutoReconnect, e:
-                    logging.error('AutoReconnect fail\n')
-                    time.sleep(2)
+    pass
 
