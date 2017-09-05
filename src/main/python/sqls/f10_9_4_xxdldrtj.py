48e2b4b88ec04c1a852da086cac140dc8857b3a7
Chen <cs1318@tom.com>
2017-09-05 16:51:56 +0800

    9.4转存储过程调用

--- src/main/python/sqls/f10_9_4_xxdldrtj.py
+++ src/main/python/sqls/f10_9_4_xxdldrtj.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 # 库：stocksoftdata 
 __author__ = "Cyh"
-sql = """SELECT TOP 500 CAST(RsID AS NVARCHAR(MAX)) AS RsId, UpdateDateTime, Obj, ZxDate,stocksoftdata.dbo.Date2Str( ZxDate ) AS II_ZxDate,
-       CountNum, ZxType,Status
-  FROM stocksoftdata.dbo.F10_9_4_XXDLDRTJ
- WHERE (   RsId > '{RsId}'
-     AND   UpdateDateTime = '{UpdateDateTime}'
-       )
-    OR UpdateDateTime > '{UpdateDateTime}'"""
+sql = """
+EXEC stocksoftdata.dbo.poc_to_mdb_f10_9_4_xxdldrtj @RsId = '{RsId}', @UpdateDateTime = '{UpdateDateTime}'
+'"""
