ab1ccccfad2e9a7ec8a756a07502e4b25b9e6b12
Chen <cs1318@tom.com>
2017-09-06 16:04:51 +0800

    8.1字段命名规范

--- src/main/python/sqls/f10_8_1_tbts.py
+++ src/main/python/sqls/f10_8_1_tbts.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 __author__ = "Sommily"
-sql = """SELECT TOP 500 a.SEQ AS Seq, a.MTIME AS Mtime,QKTZ20160429.dbo.Date2Str( a.MTIME ) AS II_Mtime, CASE x.TRADE_MKT_REF
-                                                   WHEN 1 THEN 'SZ'
-                                                   WHEN 2 THEN 'SH'
-                                                   ELSE ''
-                                               END + x.STOCKCODE AS Obj,
+sql = """
+SELECT TOP 500 a.SEQ AS RsId, a.MTIME AS UpdateDateTime,
+       QKTZ20160429.dbo.Date2Str( a.MTIME ) AS II_Mtime, CASE x.TRADE_MKT_REF
+                                                             WHEN 1 THEN 'SZ'
+                                                             WHEN 2 THEN 'SH'
+                                                             ELSE ''
+                                                         END + x.STOCKCODE AS Obj,
        CASE x.TRADE_MKT_REF
            WHEN 1 THEN 'SZ'
            WHEN 2 THEN 'SH'
            ELSE ''
-       END MarketCode, x.STOCKCODE StockCode
-	   , cast(x.STOCKSNAME as nvarchar(max)) as StockName
-	   , a.HINT_TYPE_CODE AS HintTypeCode
-	   , cast(a.HINT_TYPE_NAME as nvarchar(max)) AS HintTypeName
-	   , a.HINT_DT as HintDt
-	   ,QKTZ20160429.dbo.Date2Str(a.HINT_DT) AS II_HintDt
-	   , cast(a.ESP_HINT as nvarchar(max)) as EspHint
-	   , a.DECLAREDATE DeclareDate
-	   ,QKTZ20160429.dbo.Date2Str( a.DECLAREDATE ) AS II_DeclareDate
+       END MarketCode, x.STOCKCODE StockCode,
+       CAST(x.STOCKSNAME AS NVARCHAR(MAX)) AS StockName,
+       a.HINT_TYPE_CODE AS HintTypeCode,
+       CAST(a.HINT_TYPE_NAME AS NVARCHAR(MAX)) AS HintTypeName, a.HINT_DT AS HintDt,
+       QKTZ20160429.dbo.Date2Str( a.HINT_DT ) AS II_HintDt,
+       CAST(a.ESP_HINT AS NVARCHAR(MAX)) AS EspHint, a.DECLAREDATE DeclareDate,
+       QKTZ20160429.dbo.Date2Str( a.DECLAREDATE ) AS II_DeclareDate
   FROM NEWS_ESP_HINT AS a
   INNER JOIN STK_CODE AS x
           ON a.INNER_CODE = x.INNER_CODE
@@ -31,4 +31,5 @@ sql = """SELECT TOP 500 a.SEQ AS Seq, a.MTIME AS Mtime,QKTZ20160429.dbo.Date2Str
         )
       OR ( a.MTIME > '{UpdateDateTime}' )
        )
- ORDER BY a.MTIME, a.SEQ"""
+ ORDER BY a.MTIME, a.SEQ
+"""
